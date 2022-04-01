c2bd51fd729e79960996ecd28b61061c0997f9c7
Michael Schmid <michael.schmid@stud.edubs.chgit config --global push.default simplegit config --global user.email michael.schmid@stud.edubs.ch>
2022-04-01 12:08:08 +0200

    xvlc

--- index.js
+++ index.js
@@ -9,32 +9,15 @@ new Cam({
 }, function(err) {
   this.absoluteMove({x: 1, y: 1, zoom: 1});
   this.getStreamUri({protocol:'RTSP'}, function(err, stream) {
+
     http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/html'});
-
-      rtspstream = require('node-rtsp-stream')
-  Rtsp = new rtspstream({
-  name: 'name',
-  streamUrl: stream.uri,
-  wsPort: 9999,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
-  }
-})
-
-      res.end('<html><body>' +
-        '<canvas id="canvas" width="200" height="100"></canvas>' +
-        
+      res.write('<html><body>' + stream.uri +
+        '<embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed>' +
+        '<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" name="vlc" width="720" height="780" target="rtsp://192.168.1.96:554/live0.264" />'+
         '</body></html>');
-
         
-        /*
-        player = new jsmpeg.Player('ws://localhost:9999', {
-          canvas: document.getElementById('canvas') // Canvas should be a canvas DOM element
-        })	*/
-        
-    }).listen(443);
+    }).listen(80);
   });
 });
 
