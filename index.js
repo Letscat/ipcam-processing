47a774edee9ce39f1913540d3875f97b8c0b23c8
Michael Schmid <michael.schmid@stud.edubs.chgit config --global push.default simplegit config --global user.email michael.schmid@stud.edubs.ch>
2022-04-29 11:30:06 +0200

    update

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ new Cam({
   password: 'admin'
 }, function(err) {
   this.absoluteMove({x: 1, y: 1, zoom: 1});
-  this.getStreamUri({protocol:'RTSP'}, function(err, stream) {
+  this.getStreamUri({protocol:'RTSP'}, function(err, stream) { 
     http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('<html><body>' + stream.uri+
-        '<p></p><embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed>' +
+      res.write(stream.uri)
         
-        '</body></html>');
-        
+     
+        //SET THE RTSP STREAM ADDRESS HERE
+var address = "rtsp://192.168.0.101/mpeg4/1/media.3gp";
+
+
+
+    //SET THE DIV'S ID HERE
+   
+
+
+
     }).listen(443);
   });
 });
